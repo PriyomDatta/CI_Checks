57c20e61843bc4ea0daa19f146d286bffc49bf0d
PriyomDatta <priyomdat@gmail.com>
2025-05-14 08:35:49 +0000

    Initial commit of Proj structure

---
+++ Proj_000/Interface/src/intf.cpp
@@ -0,0 +1,18 @@
+#include"../inc/intf.hpp"
+
+/*Includes from other Modules */
+
+/*Implementation of the input interface functions*/
+unsigned char getdata_1(void){
+//Call function from other module
+
+}
+int getdata_2(void){
+//Call function from other module
+
+}
+void getdata_3(int *){
+//Call function from other module
+
+}
+
---
+++ Proj_000/Mod/inc/Mod_000.hpp
@@ -0,0 +1,12 @@
+/*Author:- Priyom Datta*/
+
+#ifndef MOD_000_H
+#define MOD_000_H
+
+//Implementation functions
+
+int Implementation _1(void);
+void Implementation _2(void);
+void Implementation _3(void);
+
+#endif
